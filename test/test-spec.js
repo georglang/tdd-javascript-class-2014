637031b070ebab41ff96859c9391b8da8f826882
Georg Lang <you@example.com>
2014-04-11 19:16:38 +0200

    test 3 rotate image

--- test/test-spec.js
+++ test/test-spec.js
@@ -1,10 +1,17 @@
-describe('Compass Tests', function() {
-  it('should show degree', function() {
+describe('Compass Tests', function () {
+  it('should show degree', function () {
     expect(showDegree()).toBe(0);
   });
 
-  it('should show text', function() {
+  it('should show text', function () {
     expect(showDegreeAsText()).toBe('North');
+  });
+
+  it('should rotate image', function () {
+    spyOn(Compass.prototype, 'showDegreeAsText');
+
+    Compass.prototype.rotateAndShowDegreeAsText();
+    expect(Compass.prototype.showDegreeAsText).toHaveBeenCalled();
   });
 });
 

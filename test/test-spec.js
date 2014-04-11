b9f4d137ad5633d021aa8c62334828b24b1c6a46
Georg Lang <you@example.com>
2014-04-11 18:48:00 +0200

    test 2 showDegreeAsText

--- test/test-spec.js
+++ test/test-spec.js
@@ -2,6 +2,9 @@ describe('Compass Tests', function() {
   it('should show degree', function() {
     expect(showDegree()).toBe(0);
   });
+  it('should show text', function() {
+    expect(showDegreeAsText()).toBe('0°');
+  });
 });
 
 function showDegree() {
